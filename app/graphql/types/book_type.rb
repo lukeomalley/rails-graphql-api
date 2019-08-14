2a21c56c6217f34c19c7dc0589b6238d77ea7e1b
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-13 20:54:22 -0400

    added user and book types and setup query types for user and users

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -2,5 +2,6 @@
 
 module Types
   class BookType < Types::BaseObject
+    field :title, String, null: false
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+  5.times do
+    Book.create(title: Faker::Book.title, user: user)
+  end
+end
