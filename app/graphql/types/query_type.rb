f03aef14c7d4a5f41133c065bd7059cd5d932c99
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-13 21:23:24 -0400

    added create_user mutation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,11 +10,11 @@ module Types
       User.all
     end
 
-    field :user, [Types::UserType], null: false do
+    field :user, Types::UserType, null: false do
       argument :id, ID, required: true
     end
 
-    def user(id: )
+    def user(id:)
       User.find(id)
     end
   end
